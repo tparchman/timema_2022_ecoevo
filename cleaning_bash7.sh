7226db57d6405ecd38983abf1218ffe0e642ddc0
tparchman <65676283+tparchman@users.noreply.github.com>
2022-06-11 11:59:30 -0700

    commit

---
+++ cleaning_bash7.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+##########################################################################################
+## T5_PC cleaning 
+##########################################################################################
+
+/working/jahner/tapioca/src/tap_contam_analysis --db  /archive/parchman_lab/rawdata_to_backup/contaminants/illumina_oligos --pct 20 /working/parchman/Tcristinae_2022/T5_S1_L002_R1_001.fastq > B_T5_PC.readstofilter.ill.txt 
+
+echo "Illumina filtering done for lane 1"
+
+/working/jahner/tapioca/src/tap_contam_analysis --db /archive/parchman_lab/rawdata_to_backup/contaminants/phix174 --pct 80 /working/parchman/Tcristinae_2022/T5_S1_L002_R1_001.fastq > B_T5_PC.readstofilter.phix.txt 
+
+echo "PhiX filtering done for lane 1"
+
+
+/working/jahner/tapioca/src/tap_contam_analysis --db  /archive/parchman_lab/rawdata_to_backup/contaminants/ecoli-k-12 --pct 80 /working/parchman/Tcristinae_2022/T5_S1_L002_R1_001.fastq > B_T5_PC.readstofilter.ecoli.txt
+
+echo "ecoli filtering done for lane 1"
+
+
+cat /working/parchman/Tcristinae_2022/T5_S1_L002_R1_001.fastq | fqu_cull -r B_T5_PC.readstofilter.ill.txt B_T5_PC.readstofilter.phix.txt B_T5_PC.readstofilter.ecoli.txt > B_T5_PC.clean.fastq
+
+echo "Clean copy of lane 1 done"
+
+
